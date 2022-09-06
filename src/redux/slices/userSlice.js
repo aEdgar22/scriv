e4a32c79b98b84b594287d86d1588e7f2ab12b93
edgar <a.edgar2209@gmail.com>
2022-09-06 16:06:22 -0500

    add/agregando carpetas y archivos faltantes

---
+++ src/redux/slices/userSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const userSlice = createSlice({
+ 
+  reducers: {}
+});
+
+export const {} = userSlice.actions
+
+export default userSlice.reducer
