30edfa82e58c208345c91543a464f140fc385564
edgar <a.edgar2209@gmail.com>
2022-09-08 14:17:32 -0500

    solucionando errores de package.json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import { Provider } from "react-redux";
 
 import { AppRouter } from "./routes/AppRouter";
-<<<<<<< HEAD
 import { GlobalStyle } from "./styleComponents/GlobalStyles";
 
-function App() {
-  return (
-    <>
-    <GlobalStyle />
-    <AppRouter />
-    </>
-  )
-=======
 import { store } from "./redux/store";
 
 function App() {
   return (
     <Provider store={store}>
+      <GlobalStyle />
       <AppRouter />
     </Provider>
   );
->>>>>>> f50623ba2278196a97cc9db047c89920389f63be
 }
 
 export default App;
