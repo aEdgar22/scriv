b64e81b0395820edbf9251c1a72a930e0885c56c
Eibeel <abelguardop@gmail.com>
2022-09-18 12:37:44 -0500

    add/animaciones entre pantallas

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -3,11 +3,12 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { LoginScreen } from "../pages/auth/login/LoginScreen";
 import { RegisterScreen } from "../pages/auth/register/RegisterScreen";
 import { Wrapper } from "../styleComponents/components/authContainerStyled";
+import 'animate.css';
 
 export const AuthRouter = () => {
   return (
     <>
-      <Wrapper>
+      <Wrapper className="animate__animated animate__fadeIn">
         <Routes>
           {/* rutas hijas */}
           <Route path="/auth/login" element={<LoginScreen />} />
