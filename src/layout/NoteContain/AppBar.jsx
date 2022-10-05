45f4f219649efa64e27f5f293fd0db3d8ae1d2a7
edgar <a.edgar2209@gmail.com>
2022-10-05 14:08:06 -0500

    add/fecha de la nota en appbar

--- src/layout/NoteContain/AppBar.jsx
+++ src/layout/NoteContain/AppBar.jsx
@@ -1,17 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
 import { startSaveNote } from "../../redux/thunks/notesThunk";
 import { NotesAppBar } from "./styledComponents/AppBarStyled";
+import moment from "moment";
 
 export const AppBar = () => {
   const dispatch = useDispatch();
   const { active: note } = useSelector((state) => state.notes);
+
+  const noteDate = moment(note.createdAt);
+
   const handleSave = () => {
     dispatch(startSaveNote(note));
   };
 
   return (
     <NotesAppBar>
-      <span>22 September of 2022</span>
+      <span>{noteDate.format( "D MMMM YYYY")}</span>
 
       <div>
         <button onClick={handleSave}>save scriv</button>
